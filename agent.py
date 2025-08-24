d258ef3bc9ce12e30d609608f90fc24f3327a0c8
Elizabeth <lizbeepmc@gmail.com>
2025-08-24 12:26:09 +0300

    open weather api integrated

--- agent.py
+++ agent.py
@@ -3,29 +3,35 @@ from zoneinfo import ZoneInfo
 from google.adk.agents import Agent 
 import requests 
 
+import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Get API key
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+
 def get_weather(city: str) -> dict:
-    """Retrieves the current weather report for a specified city.
-
-    Args:
-        city (str): The name of the city for which to retrieve the weather report.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-    if city.lower() == "new york":
-        return {
-            "status": "success",
-            "report": (
-                "The weather in New York is sunny with a temperature of 25 degrees"
-                " Celsius (77 degrees Fahrenheit)."
-            ),
-        }
-    else:
-        return {
-            "status": "error",
-            "error_message": f"Weather information for '{city}' is not available.",
-        }
-
+    """Retrieves the current weather report for a specified city using OpenWeatherMap API."""
+    try:
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        data = response.json()
+        
+        if response.status_code != 200:
+            return {"status":"error", "error_message": data.get("message", "API error")}
+        
+        # Extract info
+        temp = data["main"] ["temp"]
+        description = data["weather"][0]["description"]
+        country = data["sys"]["country"]
+        
+        report = f"The weather in {city}, {country} is {description} with a temperature of {temp}°C."
+        return {"status": "success", "report": report}
+    except Exception as e:
+        return {"status": "error", "error_message": str(e)}
 
 def get_current_time(city: str) -> dict:
     """Returns the current time in a specified city.
@@ -62,7 +68,8 @@ root_agent = Agent(
         "Agent to answer questions about the time and weather in a city."
     ),
     instruction=(
-        "You are a helpful agent who can answer user questions about the time and weather in a city.If users ask you weather in Lagos say weather is 25 degrees celsius, "
+        "You are a helpful agent who can answer user questions about the time and weather in a city. "
     ),
     tools=[get_weather, get_current_time],
 )
+print(get_weather("Lagos"))
